e40bbdf7a1b043258905ce3a6c69b7199dc49f75
rupert.avery <rupert.avery@gmail.com>
2015-07-31 19:10:37 +0800

    Clean up drawing

--- GameBoard/BaseGameRunner.cs
+++ GameBoard/BaseGameRunner.cs
@@ -80,7 +80,7 @@ namespace GameBoard
 
         private void ContainerOnKeyDown(object sender, KeyEventArgs keyEventArgs)
         {
-            keyState[(int) keyEventArgs.KeyCode] = KeyState.KeyDown;
+            keyState[(int)keyEventArgs.KeyCode] = KeyState.KeyDown;
         }
 
         private void ContainerOnKeyUp(object sender, KeyEventArgs keyEventArgs)
@@ -107,40 +107,31 @@ namespace GameBoard
 
         protected void ShowTitle(float alpha)
         {
-            Dispatcher.Invoke(() =>
+            using (var g = Graphics.FromImage(canvas))
             {
-                if (!container.IsDisposed)
+                g.Clear(Color.Black);
+                var cm = new ColorMatrix
                 {
-                    using (var g = Graphics.FromImage(canvas))
-                    {
-                        g.Clear(Color.Black);
-                        var cm = new ColorMatrix
-                        {
-                            Matrix00 = 1,
-                            Matrix11 = 1,
-                            Matrix22 = 1,
-                            Matrix44 = 1,
-                            Matrix33 = alpha
-                        };
+                    Matrix00 = 1,
+                    Matrix11 = 1,
+                    Matrix22 = 1,
+                    Matrix44 = 1,
+                    Matrix33 = alpha
+                };
 
-                        // Create a new image attribute object and set the color matrix to
-                        // the one just created
-                        var  ia = new ImageAttributes();
-                        ia.SetColorMatrix(cm);
+                // Create a new image attribute object and set the color matrix to
+                // the one just created
+                var ia = new ImageAttributes();
+                ia.SetColorMatrix(cm);
 
-                        g.DrawImage(titleBitmap, new Rectangle((CanvasWidth - titleBitmap.Width) / 2, (CanvasHeight - titleBitmap.Height) / 2, titleBitmap.Width, titleBitmap.Height), 0, 0, titleBitmap.Width, titleBitmap.Height, GraphicsUnit.Pixel, ia);
-                    }
-                    using (var f = Graphics.FromHwnd(container.Handle))
-                    {
-                        f.DrawImage(canvas, 0, 0);
-                    }
-                }
-            });
+                g.DrawImage(titleBitmap, new Rectangle((CanvasWidth - titleBitmap.Width) / 2, (CanvasHeight - titleBitmap.Height) / 2, titleBitmap.Width, titleBitmap.Height), 0, 0, titleBitmap.Width, titleBitmap.Height, GraphicsUnit.Pixel, ia);
+            }
+            RenderCanvas();
         }
 
         protected bool KeyIsDown(Keys key)
         {
-            return keyState[(int) key] == KeyState.KeyDown;
+            return keyState[(int)key] == KeyState.KeyDown;
         }
 
         protected void GameLoop()
@@ -186,6 +177,30 @@ namespace GameBoard
 
         private void Draw()
         {
+            using (var g = Graphics.FromImage(canvas))
+            {
+                g.Clear(Color.Black);
+                if (ShowFPS)
+                {
+                    g.DrawString(string.Format("{0}", fps), new Font(new FontFamily("Consolas"), 12, FontStyle.Regular), Brushes.White, 0, 0);
+                }
+
+                DrawObjects(g);
+
+                if (s.ElapsedMilliseconds >= 1000)
+                {
+                    fps = fpsctr;
+                    fpsctr = 0;
+                    s.Reset();
+                }
+
+                fpsctr++;
+            }
+            RenderCanvas();
+        }
+
+        protected void RenderCanvas()
+        {
             // Whenever we access objects created on the UI thread, we need to call it inside Dispatcher.Invoke
             // The Dispatcher must have been retrieved from the UI thread's Dispatcher.CurrentDispatcher
             Dispatcher.Invoke(() =>
@@ -194,28 +209,9 @@ namespace GameBoard
                 // and it will try to draw to the disposed container
                 if (!container.IsDisposed)
                 {
-                    using (var g = Graphics.FromImage(canvas))
-                    {
-                        g.Clear(Color.Black);
-                        if (ShowFPS)
-                        {
-                            g.DrawString(string.Format("{0}", fps), new Font(new FontFamily("Consolas"), 12, FontStyle.Regular), Brushes.White, 0, 0);
-                        }
-
-                        DrawObjects(g);
-
-                        if (s.ElapsedMilliseconds >= 1000)
-                        {
-                            fps = fpsctr;
-                            fpsctr = 0;
-                            s.Reset();
-                        }
-
-                        fpsctr++;
-                    }
                     using (var f = Graphics.FromHwnd(container.Handle))
                     {
-                        f.DrawImage(canvas, 0, 0);
+                        f.DrawImageUnscaled(canvas, 0, 0);
                     }
                 }
             });
